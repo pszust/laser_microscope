e9447bf547202b8228c005892d70ffdbf543409d
piotr <szustakpio@gmail.com>
2024-11-10 18:24:21 +0100

    Slowly moving forward

---
+++ main.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+from main_window import MainWindow
+
+def main():
+    root = tk.Tk()
+    root.geometry("1920x1080")
+    # root.attributes('-fullscreen', True)
+    root.state('zoomed')
+    root.title("Laser Microscope Control")
+    app = MainWindow(master=root)
+    app.mainloop()
+
+if __name__ == "__main__":
+    main()
